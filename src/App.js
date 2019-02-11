f9a8100c2082a6817ca94b5a24b57cb0653b11b4
Philihp Busby <philihp@gmail.com>
2019-02-11 01:12:56 -0800

    add eslint plugisn

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './app.css';
 
-export default () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+import { Client } from 'boardgame.io/react';
+import { Game } from 'boardgame.io/core';
+
+const BetweenTwoCities = Game({
+  setup: () => ({ game: [] }),
+
+  moves: {
+    clickCell: (G, ctx, id) => ({ ...G }),
+  },
+});
+
+const App = Client({ game: BetweenTwoCities });
+
+export default App;
